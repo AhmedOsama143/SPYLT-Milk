d442765a2033588671aaf845c648d0575a5039fb
AhmedOsama143 <ao8911323@gmail.com>
2025-10-15 05:45:43 +0300

    Fix some errors about imgs and videos

--- src/components/FlavorSlider.jsx
+++ src/components/FlavorSlider.jsx
@@ -1,4 +1,3 @@
-
 import { useGSAP } from "@gsap/react";
 import { flavorlists } from "../constants";
 import gsap from "gsap";
@@ -8,102 +7,99 @@ import { ScrollTrigger } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger);
 
 const FlavorSlider = () => {
-    const sliderRef = useRef();
+  const sliderRef = useRef();
 
-    useGSAP(() => {
-        ScrollTrigger.matchMedia({
-            // Desktop (horizontal scroll)
-            "(min-width: 1025px)": () => {
-                const scrollAmount =
-                    sliderRef.current.scrollWidth - window.innerWidth;
+  useGSAP(() => {
+    ScrollTrigger.matchMedia({
+      "(min-width: 1025px)": () => {
+        const scrollAmount = sliderRef.current.scrollWidth - window.innerWidth;
 
-                const tl = gsap.timeline({
-                    scrollTrigger: {
-                        trigger: ".flavor-section",
-                        start: "2% top",
-                        end: `+=${scrollAmount + 1500}px`,
-                        scrub: true,
-                        pin: true,
-                    },
-                });
-
-                tl.to(".flavor-section", {
-                    x: `-${scrollAmount + 1500}px`,
-                    ease: "power1.inOut",
-                });
-            },
-
-            // Tablet & Mobile (no horizontal scroll)
-            "(max-width: 1024px)": () => {
-                gsap.set(".flavor-section", { clearProps: "all" });
-            },
+        const tl = gsap.timeline({
+          scrollTrigger: {
+            trigger: ".flavor-section",
+            start: "2% top",
+            end: `+=${scrollAmount + 1500}px`,
+            scrub: true,
+            pin: true,
+          },
         });
 
-        // Title scroll animations (applies everywhere)
-        const titleTl = gsap.timeline({
-            scrollTrigger: {
-                trigger: ".flavor-section",
-                start: "top top",
-                end: "bottom 80%",
-                scrub: true,
-            },
+        tl.to(".flavor-section", {
+          x: `-${scrollAmount + 1500}px`,
+          ease: "power1.inOut",
         });
+      },
 
-        titleTl
-            .to(".first-text-split", {
-                xPercent: -30,
-                ease: "power1.inOut",
-            })
-            .to(
-                ".flavor-text-scroll",
-                {
-                    xPercent: -22,
-                    ease: "power1.inOut",
-                },
-                "<"
-            )
-            .to(
-                ".second-text-split",
-                {
-                    xPercent: -10,
-                    ease: "power1.inOut",
-                },
-                "<"
-            );
+      "(max-width: 1024px)": () => {
+        gsap.set(".flavor-section", { clearProps: "all" });
+      },
     });
 
-    return (
-        <div ref={sliderRef} className="slider-wrapper">
-            <div className="flavors">
-                {flavorlists.map((flavor) => (
-                    <div
-                        key={flavor.name}
-                        className={`relative z-30 lg:w-[50vw] w-96 lg:h-[70vh] md:w-[90vw] md:h-[50vh] h-80 flex-none ${flavor.rotation}`}
-                    >
-                        <img
-                            src={`/images/${flavor.color}-bg.svg`}
-                            alt=""
-                            className="absolute bottom-0"
-                        />
+    // Title scroll animations (applies everywhere)
+    const titleTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".flavor-section",
+        start: "top top",
+        end: "bottom 80%",
+        scrub: true,
+      },
+    });
 
-                        <img
-                            src={`/images/${flavor.color}-drink.webp`}
-                            alt=""
-                            className="drinks"
-                        />
+    titleTl
+      .to(".first-text-split", {
+        xPercent: -30,
+        ease: "power1.inOut",
+      })
+      .to(
+        ".flavor-text-scroll",
+        {
+          xPercent: -22,
+          ease: "power1.inOut",
+        },
+        "<"
+      )
+      .to(
+        ".second-text-split",
+        {
+          xPercent: -10,
+          ease: "power1.inOut",
+        },
+        "<"
+      );
+  });
 
-                        <img
-                            src={`/images/${flavor.color}-elements.webp`}
-                            alt=""
-                            className="elements"
-                        />
+  return (
+    <div ref={sliderRef} className="slider-wrapper">
+      <div className="flavors">
+        {flavorlists.map((flavor) => (
+          <div
+            key={flavor.name}
+            className={`relative z-30 lg:w-[50vw] w-96 lg:h-[70vh] md:w-[90vw] md:h-[50vh] h-80 flex-none ${flavor.rotation}`}
+          >
+            <img
+              src={`images/${flavor.color}-bg.svg`}
+              alt=""
+              className="absolute bottom-0"
+            />
 
-                        <h1>{flavor.name}</h1>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+            <img
+              src={`images/${flavor.color}-drink.webp`}
+              alt=""
+              className="drinks"
+            />
+
+            <img
+              src={`images/${flavor.color}-elements.webp`}
+              alt=""
+              className="elements"
+            />
+
+            <h1>{flavor.name}</h1>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default FlavorSlider;
--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -68,18 +68,18 @@ const HeroSection = () => {
           <>
             {isMobile && (
               <img
-                src="/images/hero-bg.png"
+                src="images/hero-bg.png"
                 className="absolute bottom-40 size-full object-cover"
               />
             )}
             <img
-              src="/images/hero-img.png"
+              src="images/hero-img.png"
               className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto"
             />
           </>
         ) : (
           <video
-            src="/videos/hero-bg.mp4"
+            src="videos/hero-bg.mp4"
             autoPlay
             muted
             playsInline
@@ -106,7 +106,7 @@ const HeroSection = () => {
             your inner kid with every deliciously smooth chug.
           </h2>
 
-          <div className="hero-button">
+          <div className="hero-button cursor-pointer">
             <p>Chug a SPYLT</p>
           </div>
         </div>
